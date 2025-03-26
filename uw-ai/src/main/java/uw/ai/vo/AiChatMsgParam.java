e8efa84d97c6f49d94fa15ea21600fc6302bd7cc
zhangjin <23231269@qq.com>
2025-03-26 11:40:51 +0800

    feat(ai): 增加工具上下文参数并调整相关接口- 在 AiChatGenerateParam 和 AiChatMsgParam 类中添加 toolContext 字段 - 修改 AiChatService 中的 generate 和 chat 方法，增加 toolContext 参数 - 更新相关控制器中的 generate 和 chat 方法，支持新的 toolContext 参数 - 调整 toolContext 的构建逻辑，支持自定义上下文信息

--- uw-ai/src/main/java/uw/ai/vo/AiChatMsgParam.java
+++ uw-ai/src/main/java/uw/ai/vo/AiChatMsgParam.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * AiChat聊天消息参数。
@@ -35,6 +36,11 @@ public class AiChatMsgParam {
     @Schema(title = "工具列表", description = "工具列表", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
     private List<AiToolCallInfo> toolList;
 
+    /**
+     * 工具上下文。
+     */
+    @Schema(title = "工具上下文", description = "工具上下文", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    private Map<String,Object> toolContext;
 
     /**
      * 文件列表。
@@ -75,6 +81,14 @@ public class AiChatMsgParam {
 
     public void setToolList(List<AiToolCallInfo> toolList) {
         this.toolList = toolList;
+    }
+
+    public Map<String, Object> getToolContext() {
+        return toolContext;
+    }
+
+    public void setToolContext(Map<String, Object> toolContext) {
+        this.toolContext = toolContext;
     }
 
     public MultipartFile[] getFileList() {

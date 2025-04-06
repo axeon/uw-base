ecd2c36a6d8e8058c2e5c16dc7978cd313cf2e96
zhangjin <23231269@qq.com>
2025-04-06 23:12:15 +0800

    refactor(auth): 重构登录日志相关代码
    - 为 MscGuestLoginLog 和 MscLoginLog 类添加 Swagger 注解
    - 优化类结构，重新排序字段顺序
    - 修改部分字段名称，使其更加清晰
    - 删除冗余的 getter 和 setter 方法

--- uw-auth-service/src/main/java/uw/auth/service/vo/MscLoginLog.java
+++ uw-auth-service/src/main/java/uw/auth/service/vo/MscLoginLog.java
@@ -1,5 +1,6 @@
 package uw.auth.service.vo;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import uw.log.es.vo.LogBaseVo;
 
 import java.io.Serializable;
@@ -14,91 +15,114 @@ public class MscLoginLog extends LogBaseVo implements Serializable {
     /**
      * 登录客户端。
      */
+    @Schema(title = "登录客户端", description = "登录客户端")
     private String loginAgent;
 
     /**
      * 登录类型。
      */
+    @Schema(title = "登录类型", description = "登录类型" )
     private int loginType;
 
     /**
      * 登录标识，用户名/手机号/email地址。
      */
+    @Schema(title = "登录标识", description = "登录标识" )
     private String loginId;
 
     /**
      * 用户Id
      */
+    @Schema(title = "用户Id", description = "用户Id" )
     private long userId;
 
     /**
-     * 用户类型 ==> msc_user_type.type_code
+     * 用户信息。
      */
-    private int userType;
-
-    /**
-     * 运营商Id
-     */
-    private long saasId;
-
-    /**
-     * 分销商Id
-     */
-    private long mchId;
-
-    /**
-     * 用户组Id。
-     */
-    private long groupId;
-
-    /**
-     * 用户登录名
-     */
+    @Schema(title = "用户信息", description = "用户信息" )
     private String userName;
 
     /**
      * 昵称信息。
      */
+    @Schema(title = "昵称信息", description = "昵称信息" )
     private String nickName;
 
     /**
      * 真实姓名。
      */
+    @Schema(title = "真实姓名", description = "真实姓名" )
     private String realName;
+    /**
+     * 运营商Id
+     */
+    @Schema(title = "运营商Id", description = "运营商Id" )
+    private long saasId;
+
+    /**
+     * 商户Id
+     */
+    @Schema(title = "商户Id", description = "商户Id" )
+    private long mchId;
+
+    /**
+     * 用户组Id。
+     */
+    @Schema(title = "用户组Id", description = "用户组Id" )
+    private long groupId;
+
+    /**
+     * 用户类型 ==> msc_user_type.type_code
+     */
+    @Schema(title = "用户类型", description = "用户类型" )
+    private int userType;
 
     /**
      * 登录IP
      */
+    @Schema(title = "登录IP", description = "登录IP" )
     private String userIp;
 
     /**
      * 用户登录设备类型: 1: PC-WEB; 2: MOBILE-WEB; 3: MOBILE-APP; 4: WEIXIN-APP etc
      */
+    @Schema(title = "登录设备", description = "登录设备" )
     private int clientType;
 
     /**
      * 用户代理
      */
+    @Schema(title = "用户代理", description = "用户代理" )
     private String clientAgent;
 
     /**
-     * 登录结果 按照http code进行。
+     * 响应状态
      */
-    private int loginResult;
+    @Schema(title = "响应状态", description = "响应状态" )
+    private String responseState;
 
     /**
-     * 备注: 失败原因,成功备注
+     * 响应代码
      */
-    private String remark;
+    @Schema(title = "响应代码", description = "响应代码" )
+    private String responseCode;
+
+    /**
+     * 响应消息
+     */
+    @Schema(title = "响应消息", description = "响应消息" )
+    private String responseMsg;
 
     /**
      * 登录时间
      */
-    private java.util.Date loginDate;
+    @Schema(title = "登录时间", description = "登录时间" )
+    private Date loginDate;
 
     /**
      * 执行返回毫秒数。
      */
+    @Schema(title = "响应毫秒数", description = "响应毫秒数" )
     private long responseMillis;
 
     public String getLoginAgent() {
@@ -107,14 +131,6 @@ public class MscLoginLog extends LogBaseVo implements Serializable {
 
     public void setLoginAgent(String loginAgent) {
         this.loginAgent = loginAgent;
-    }
-
-    public Date getLoginDate() {
-        return loginDate;
-    }
-
-    public void setLoginDate(Date loginDate) {
-        this.loginDate = loginDate;
     }
 
     public int getLoginType() {
@@ -141,12 +157,28 @@ public class MscLoginLog extends LogBaseVo implements Serializable {
         this.userId = userId;
     }
 
-    public int getUserType() {
-        return userType;
+    public String getUserName() {
+        return userName;
     }
 
-    public void setUserType(int userType) {
-        this.userType = userType;
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getRealName() {
+        return realName;
+    }
+
+    public void setRealName(String realName) {
+        this.realName = realName;
     }
 
     public long getSaasId() {
@@ -173,28 +205,12 @@ public class MscLoginLog extends LogBaseVo implements Serializable {
         this.groupId = groupId;
     }
 
-    public String getUserName() {
-        return userName;
+    public int getUserType() {
+        return userType;
     }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getNickName() {
-        return nickName;
-    }
-
-    public void setNickName(String nickName) {
-        this.nickName = nickName;
-    }
-
-    public String getRealName() {
-        return realName;
-    }
-
-    public void setRealName(String realName) {
-        this.realName = realName;
+    public void setUserType(int userType) {
+        this.userType = userType;
     }
 
     public String getUserIp() {
@@ -221,21 +237,36 @@ public class MscLoginLog extends LogBaseVo implements Serializable {
         this.clientAgent = clientAgent;
     }
 
-
-    public int getLoginResult() {
-        return loginResult;
+    public String getResponseState() {
+        return responseState;
     }
 
-    public void setLoginResult(int loginResult) {
-        this.loginResult = loginResult;
+    public void setResponseState(String responseState) {
+        this.responseState = responseState;
     }
 
-    public String getRemark() {
-        return remark;
+    public String getResponseCode() {
+        return responseCode;
     }
 
-    public void setRemark(String remark) {
-        this.remark = remark;
+    public void setResponseCode(String responseCode) {
+        this.responseCode = responseCode;
+    }
+
+    public String getResponseMsg() {
+        return responseMsg;
+    }
+
+    public void setResponseMsg(String responseMsg) {
+        this.responseMsg = responseMsg;
+    }
+
+    public Date getLoginDate() {
+        return loginDate;
+    }
+
+    public void setLoginDate(Date loginDate) {
+        this.loginDate = loginDate;
     }
 
     public long getResponseMillis() {

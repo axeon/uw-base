05dee87c246ca0513b537ff754cfdd50813d6273
zhangjin <23231269@qq.com>
2025-04-15 20:06:50 +0800

    refactor(auth-service): 优化异常处理逻辑
    - 移除了对特定异常类型的处理，简化了异常处理逻辑
    - 优化了错误信息的处理方式，提高了错误信息的可读性
    - 更新了版本号：
      - uw-auth-service.version: 6.1.11 -> 6.1.12
      - uw-mfa.version: 1.0.35 -> 1.0.36
    - 修改了revision: 2025.0330.1022 -> 2025.0330.1023

--- uw-auth-service/src/main/java/uw/auth/service/advice/GlobalExceptionAdvice.java
+++ uw-auth-service/src/main/java/uw/auth/service/advice/GlobalExceptionAdvice.java
@@ -4,24 +4,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.TypeMismatchException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.http.converter.HttpMessageNotWritableException;
 import org.springframework.web.ErrorResponse;
-import org.springframework.web.HttpMediaTypeNotAcceptableException;
-import org.springframework.web.HttpMediaTypeNotSupportedException;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.MissingPathVariableException;
-import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.async.AsyncRequestTimeoutException;
-import org.springframework.web.servlet.resource.NoResourceFoundException;
-import uw.auth.service.AuthServiceHelper;
 import uw.auth.service.util.MscUtils;
-import uw.auth.service.vo.MscActionLog;
 import uw.common.dto.ResponseData;
 
 /**
@@ -29,31 +16,29 @@ import uw.common.dto.ResponseData;
  */
 @RestControllerAdvice
 public class GlobalExceptionAdvice {
-    private static final Logger log = LoggerFactory.getLogger( GlobalExceptionAdvice.class );
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionAdvice.class);
 
     public GlobalExceptionAdvice() {
-        log.info( "Init GlobalExceptionAdvice." );
+        log.info("Init GlobalExceptionAdvice.");
     }
 
     @ExceptionHandler({Throwable.class})
     public ResponseData<String> exceptionHandle(Throwable ex, HttpServletRequest request, HttpServletResponse response) {
         // 针对ErrorResponse异常，设置不同的状态码。
         if (ex instanceof ErrorResponse errorResponse) {
-            response.setStatus( errorResponse.getStatusCode().value() );
-            log.warn( ex.getMessage() );
-        } else {
-            response.setStatus( HttpStatus.INTERNAL_SERVER_ERROR.value() );
-            //500类异常，要打印到日志里。
-            log.error( ex.getMessage(), ex );
+            response.setStatus(errorResponse.getStatusCode().value());
         }
         //针对不同类型异常，设置不同的详细消息。
-        String detailData;
+        String detailData = null;
         if (response.getStatus() == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-            detailData = MscUtils.exceptionToString( ex );
+            //500类异常，要打印到日志里。
+            log.error(ex.getMessage(), ex);
+            detailData = MscUtils.exceptionToString(ex);
         } else {
-            detailData = ex.getMessage();
+            //非500类异常，直接打印消息就OK。
+            log.warn(ex.getMessage());
         }
-        return ResponseData.error( detailData, "http.status." + String.valueOf( response.getStatus() ), ex.getMessage() );
+        return ResponseData.error(detailData, "http.status." + response.getStatus(), ex.getMessage());
     }
 
 }

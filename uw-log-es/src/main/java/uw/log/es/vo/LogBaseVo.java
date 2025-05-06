f0ed8c6ad2529651837c8dad905cae12ff2e4cf0
zhangjin <23231269@qq.com>
2025-05-06 11:40:17 +0800

    refactor(log): 优化日志相关代码和注释
    - 修改 SysCritLogStorageService 初始化日志信息
    - 在 LogBaseVo 中添加 Swagger 注释，提高接口文档可读性

--- uw-log-es/src/main/java/uw/log/es/vo/LogBaseVo.java
+++ uw-log-es/src/main/java/uw/log/es/vo/LogBaseVo.java
@@ -1,6 +1,7 @@
 package uw.log.es.vo;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 /**
  * 日志基类Vo
@@ -14,21 +15,25 @@ public abstract class LogBaseVo {
      * 记录时间,方便kibana分析
      */
     @JsonProperty("@timestamp")
+    @Schema(title = "记录时间", description = "记录时间")
     protected String timestamp;
 
     /**
      * 应用信息
      */
+    @Schema(title = "应用信息", description = "应用信息")
     protected String appInfo;
 
     /**
      * 应用主机
      */
+    @Schema(title = "应用主机", description = "应用主机")
     protected String appHost;
 
     /**
      * 记录日志的级别。
      */
+    @Schema(title = "记录日志的级别", description = "记录日志的级别")
     protected int logLevel;
 
     public String getTimestamp() {
